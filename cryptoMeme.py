80d04c71409892ff4c5dd2dfd9fc04065956bc7f
sd31117 <sd31117@hotmail.com>
2021-01-20 00:35:19 -0600

    rounded numbers and quotes around speech string so it says correct text

--- cryptoMeme.py
+++ cryptoMeme.py
@@ -52,12 +52,15 @@ while 1==1:
 
     sumValues = cryptovalue2 / cryptovalue1
 
+
+    sumValues = 1.05
+
     if sumValues >= 1.001:
         print("%s increased by 1 percent+ in the last %s mins!" % (userCoin, userMins))
         playsound("cash.wav")
 
-        percentValue = (sumValues * 100) - 100
-        speakText = 'Hello mother fucker, " + userCoin + " is down " + str(percentValue) + " percent in the last 30 mins'
+        percentValue = round(((sumValues * 100) - 100), 3)
+        speakText = '"Hello mother fucker, ' + userCoin + ' is up ' + str(percentValue) + ' percent in the last 30 mins"'
 
         #speaks information using espeak, shell must be set to True because it is not working in an emulated environment
         call('espeak '+speakText+' --stdout | paplay', shell=True)
@@ -66,7 +69,7 @@ while 1==1:
         print("%s decreased by 1 percent+ in the last %s mins!" % (userCoin, userMins))
         playsound("suffer.wav")
 
-        percentValue = (1 - sumValues) * 100
+        percentValue = round((1 - sumValues) * 100, 3)
         speakText = 'Hello mother fucker, " + userCoin + " is down " + str(percentValue) + " percent in the last 30 mins'
 
         #speaks information using espeak, shell must be set to True because it is not working in an emulated environment
