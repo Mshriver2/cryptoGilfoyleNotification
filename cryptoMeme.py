02c0e720c70473e2eef04a321eaf3e5b51beb647
Max Shriver <maxwell.shriver@yahoo.com>
2021-01-21 03:14:10 -0700

    fixed wrong percent value being shown if coin did not go up or down, added red colors for down and green colors for up

--- cryptoMeme.py
+++ cryptoMeme.py
@@ -3,6 +3,8 @@ import http.client
 import time
 from playsound import playsound
 from subprocess import call
+import colorama
+from colorama import Fore, Style
 
 print("Enter coin full name: ")
 
@@ -66,26 +68,26 @@ while 1==1:
         playsound("cash.wav")
 
         percentValue = round(((sumValues * 100) - 100), 3)
-        speakText = '"Hello mother fucker, ' + userCoin + ' is up ' + str(percentValue) + ' percent in the last 30 mins"'
+        speakText = '"Hello mother fucker, ' + userCoin + ' is up ' + str(percentValue) + ' percent in the last '+ userMins +' mins"'
 
         #speaks information using espeak, shell must be set to True because it is not working in an emulated environment
         call('espeak '+speakText+' --stdout | paplay', shell=True)
 
-        print("%s increased by %s percent in the last %s mins!" % (userCoin, percentValue, userMins))
+        print(Fore.GREEN + "%s increased by %s percent in the last %s mins!" % (userCoin, percentValue, userMins))
 
     elif sumValues <= decreasePercent:
         
         playsound("suffer.wav")
 
         percentValue = round((1 - sumValues) * 100, 3)
-        speakText = 'Hello mother fucker, " + userCoin + " is down " + str(percentValue) + " percent in the last 30 mins'
+        speakText = '"Hello mother fucker, ' + userCoin + ' is down ' + str(percentValue) + ' percent in the last '+ userMins +' mins"'
 
         #speaks information using espeak, shell must be set to True because it is not working in an emulated environment
         call('espeak '+speakText+' --stdout | paplay', shell=True)
 
-        print("%s decreased by %s percent in the last %s mins!" % (userCoin, percentValue, userMins))
+        print(Fore.RED + "%s decreased by %s percent in the last %s mins!" % (userCoin, percentValue, userMins))
 
     else:
-        print("%s did not increase or decrease by 1 percent+ in the last %s mins!\n" % (userCoin, userMins))
+        print("\n%s did not increase or decrease by %s percent in the last %s mins!\n" % (userCoin, userPercentage, userMins))
 
     print("sumValues equals %s" % (sumValues))
